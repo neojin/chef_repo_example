cb9067e43357d9c8572c011baf586b09451d7fd1
Jin Lee <jin.lee@bydeluxe.com>
2014-04-25 11:35:34 -0700

    commit knife

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'jinlee'
+client_key               '/Users/jinlee/Documents/work/chef_book/chef-repo/.chef/jinlee.pem'
+validation_client_name   'chef-validator'
+validation_key           '/Users/jinlee/Documents/work/chef_book/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://33.33.33.33'
+syntax_check_cache_path  '/Users/jinlee/Documents/work/chef_book/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '/Users/jinlee/Documents/work/chef_book/chef-repo/cookbooks' ]
